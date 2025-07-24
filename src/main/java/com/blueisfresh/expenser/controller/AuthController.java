36d5918d5e291185249836f93b3beae8659a9bf1
blueisfresh <elias.guerematchi.vrabl@gmail.com>
2025-07-24 14:45:47 +0200

    Auth Controller

---
+++ src/main/java/com/blueisfresh/expenser/controller/AuthController.java
@@ -0,0 +1,54 @@
+package com.blueisfresh.expenser.controller;
+
+import com.blueisfresh.expenser.entity.User;
+import com.blueisfresh.expenser.repository.UserRepository;
+import com.blueisfresh.expenser.security.JwtUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.*;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    @Autowired
+    AuthenticationManager authenticationManager;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    PasswordEncoder encoder;
+
+    @Autowired
+    JwtUtil jwtUtils;
+
+    @PostMapping("/signin")
+    public String authenticateUser(@RequestBody User user) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        user.getUsername(),
+                        user.getPasswordHash()
+                )
+        );
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        return jwtUtils.generateToken(userDetails.getUsername());
+    }
+    @PostMapping("/signup")
+    public String registerUser(@RequestBody User user) {
+        if (userRepository.existsByUserName(user.getUsername())) {
+            return "Error: Username is already taken!";
+        }
+        // Create new user's account
+        User newUser = new User(
+                null,
+                user.getUsername(),
+                encoder.encode(user.getPasswordHash())
+        );
+        userRepository.save(newUser);
+        return "User registered successfully!";
+    }
+}
